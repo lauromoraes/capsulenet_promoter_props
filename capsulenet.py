8577b7cd94534da84160e468df69537333dea935
Lauro Moraes <kaveira.asteca@gmail.com>
2018-02-06 14:32:49 -0200

    correction conv shape 2

--- capsulenet.py
+++ capsulenet.py
@@ -44,7 +44,7 @@ def CapsNet(input_shape, n_class, routings):
     conv1 = layers.Conv2D(filters=256, kernel_size=(1,9), strides=1, padding='valid', activation='relu', name='conv1')(x)
 
     # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
-    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=(1,9), strides=2, padding='valid')
 
     # Layer 3: Capsule layer. Routing algorithm works here.
     digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings,
